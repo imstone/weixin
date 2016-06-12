e0e8b7ca13ce75a46978c0fbc9eb06ddb06aa670
tanruixing <tanruixing@baidu.com>
2016-06-12 22:47:16 +0800

    remove config info

--- app.js
+++ app.js
@@ -6,20 +6,7 @@ var util = require('./libs/util');
 var path = require('path');
 var config = require('./config')
 var weixin = require('./weixin')
-// var wechat_file = path.join(__dirname, './config/wexin.txt');
 
-// var config = {
-//     appID: 'wx4f4bfd59e2e2d8ab',
-//     appSecret: '9008d71b8297f48b673357852945fcbb',
-//     token: 'fe178weixin',
-//     getAccessToken: function (){
-//     	return util.readFileAsyns(wechat_file)
-//     },
-//     saveAccessToken: function (data){
-//     	data = JSON.stringify(data)
-//     	return util.writeFileAsync(wechat_file,data)
-//     }
-// }
 var app = new Koa();
 app.use(wechat(config, weixin.reply))
 app.listen(1234);
